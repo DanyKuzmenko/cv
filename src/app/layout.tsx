e66762431f9da34bdb14a30403429cac36c70b1f
Kuzmenko <kuzmenko@jfservice.ru>
2025-04-16 08:02:03 +0300

    - Добавил шрифт - Добавил ссылки в шапку

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,38 +1,38 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import { Header } from '@/widgets/header'
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter'
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
+import { Roboto } from 'next/font/google'
+import { ThemeProvider } from '@mui/material/styles'
+import theme from './theme'
 
 export const metadata: Metadata = {
   title: 'Резюме Кузьменко Даниил',
   description: 'Резюме Кузьменко Даниил Frontend разработчик',
 }
 
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-roboto',
+})
+
 export default function RootLayout({
-  children,
-}: Readonly<{
+                                     children,
+                                   }: Readonly<{
   children: React.ReactNode
 }>) {
   return (
-    <html lang='ru'>
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-      <AppRouterCacheProvider>
+    <html lang="ru" className={roboto.variable}>
+    <body>
+    <AppRouterCacheProvider>
+      <ThemeProvider theme={theme}>
         <Header />
         {children}
-      </AppRouterCacheProvider>
-
-      </body>
+      </ThemeProvider>
+    </AppRouterCacheProvider>
+    </body>
     </html>
   )
 }
--- src/widgets/header/ui/index.tsx
+++ src/widgets/header/ui/index.tsx
@@ -1,7 +1,21 @@
-import { AppBar } from '@mui/material'
+import { AppBar, Button, Container, Menu, MenuItem, Toolbar, Typography } from '@mui/material'
+
+const pages = [{label: 'Обо мне', href: '/about-me'}, {label: 'Проекты', href: '/projects'}, {label: 'Ценности', href: '/interests'}]
 
 export function Header() {
   return (
-   <AppBar position={'static'}></AppBar>
+   <AppBar position={'static'}>
+     <Container maxWidth={'xl'}>
+        <Toolbar>
+          <Box>
+            {pages.map((page) => (
+              <Button key={page.label}>
+                {page.label}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+     </Container>
+   </AppBar>
   )
 }
